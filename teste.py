88c7f03b349c2a3c952b0bd6c0408d3a6500cc2a
Iago <iago.pferreiravr@gmail.com>
2023-07-10 18:17:33 -0300

    Comentando código do arquivo teste.py

--- teste.py
+++ teste.py
@@ -60,31 +60,39 @@
 
 
 # print(string('oi', ' idiota'))
-class Classe:
-    _atributo_da_classe = 1
+# class Classe:
+#     _atributo_da_classe = 1
 
-    @classmethod
-    def seta_atributo_da_classe(cls, valor):
-        cls._atributo_da_classe = valor
+#     @classmethod
+#     def seta_atributo_da_classe(cls, valor):
+#         cls._atributo_da_classe = valor
 
-    @classmethod
-    def retorna_atributo_da_classe(cls):
-        return cls._atributo_da_classe
+#     @classmethod
+#     def retorna_atributo_da_classe(cls):
+#         return cls._atributo_da_classe
 
 
-objeto_1 = Classe()
-objeto_2 = Classe()
+# objeto_1 = Classe()
+# objeto_2 = Classe()
 
-print(Classe.retorna_atributo_da_classe())  # Saída: 1
-print(objeto_1.retorna_atributo_da_classe())  # Saída: 1
-print(objeto_2.retorna_atributo_da_classe())  # Saída: 1
+# print(Classe.retorna_atributo_da_classe())  # Saída: 1
+# print(objeto_1.retorna_atributo_da_classe())  # Saída: 1
+# print(objeto_2.retorna_atributo_da_classe())  # Saída: 1
 
-Classe.seta_atributo_da_classe(2)
-print(Classe.retorna_atributo_da_classe())  # Saída: 2
-print(objeto_1.retorna_atributo_da_classe())  # Saída: 2
-print(objeto_2.retorna_atributo_da_classe())  # Saída: 2
+# Classe.seta_atributo_da_classe(2)
+# print(Classe.retorna_atributo_da_classe())  # Saída: 2
+# print(objeto_1.retorna_atributo_da_classe())  # Saída: 2
+# print(objeto_2.retorna_atributo_da_classe())  # Saída: 2
 
-objeto_1.seta_atributo_da_classe(3)
-print(Classe.retorna_atributo_da_classe())  # Saída: 3
-print(objeto_1.retorna_atributo_da_classe())  # Saída: 3
-print(objeto_2.retorna_atributo_da_classe())  # Saída: 3
+# objeto_1.seta_atributo_da_classe(3)
+# print(Classe.retorna_atributo_da_classe())  # Saída: 3
+# print(objeto_1.retorna_atributo_da_classe())  # Saída: 3
+# print(objeto_2.retorna_atributo_da_classe())  # Saída: 3
+
+# def _format_date(date):
+#     splited = date.split('-')
+#     splited.reverse()
+#     return '/'.join(splited)
+
+
+# print(_format_date('2023-07-08'))
